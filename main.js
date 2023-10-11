87a8d7cb3b326da18ebf78ed47e3ffb597765813
Juan Rodriguez <rodriguezcolmenaresjuand@gmail.com>
2023-10-11 17:10:24 -0300

    Segunda preentrega

--- main.js
+++ main.js
@@ -17,34 +17,43 @@
 let apellido = prompt("Ingresa tu apellido");
 let nombre = prompt("Ingresa tu nombre");
 
-// const productos = [
-//     { carrito: "Pizza", precio: 50},
-//     { carrito: "Pollo frito", precio: 40},
-//     { carrito: "Rolls", precio: 30},
-// ];
 
 let pregunta = prompt("Hola "+nombre+" "+apellido+" , te gustaria comprar en nuestra tienda?")
 
-while(pregunta != "si" && pregunta != "no" && pregunta != "Si" && pregunta != "No"){
+while(pregunta.toLowerCase() != "si" && pregunta.toLowerCase() != "no"){
     alert("Debe escribir si o no")
     pregunta = prompt("Hola "+nombre+" "+apellido+" , te gustaria comprar en nuestra tienda?")  
 }
 
-if(pregunta == "si" || pregunta == "Si"){
-    alert(" Disculpa "+nombre+", pronto tendremos las opciones para mostrar nuestros productos. Muchas gracias")
-}
-else if(pregunta == "no" || pregunta == "No"){
+if(pregunta.toLowerCase() == "si"){
+    alert(""+nombre+", Aca estan todos nuestros productos y nuestros precios")
+    const productos = [
+        { carrito: "Pizza muzzarella", precio: 500},
+        { carrito: "Pizza pepperoni", precio: 750},
+        { carrito: "3 piezas de Pollo frito", precio: 300},
+        { carrito: "Rolls", precio: 200},
+        { carrito: "2 Pizza muzzarela + Refresco 1lt", precio: 750},
+        { carrito: "Combo de 5 piezas de Pollo + Refresco 600ml", precio: 650},
+        {carrito: "2x1 en Pizzas(solo lunes y martes", precio: 500},
+        {carrito: "4x3 en Postres", precio: 600}
+    ];
+
+    let listaProductos = productos.map(producto => `${producto.carrito} - $${producto.precio.toFixed(2)}`).join('\n');
+
+    // La lista en un alert
+    alert("Lista de Productos:\n" + listaProductos);
+    // La lista en la consola
+     console.log("Lista de Productos:");
+        productos.forEach(function(producto) {
+            console.log(`${producto.carrito} - $${producto.precio}`);});
+}    
+
+else if(pregunta.toLowerCase() == "no"){
     alert(" Muchas gracias "+nombre+"  por pasar por nuestra tienda. Esperamos la proxima vez nos compre")
 }
 
-let cuestionario = parseInt(prompt("Cuantos años tienes?"))
-if( cuestionario >= 18){
-    alert("Eres mayor de edad, "+nombre+"")
-}
-else if( cuestionario < 18){
-    alert(" "+nombre+", Debes contar con la supervision de un adulto para poder comprar en la web")
-}
-//Fin de la consola interactiva(Preentrega 1)
+
+//Fin de la consola interactiva(Preentrega 2)
 
 
 // let edad = parseInt(prompt("Ingresa tu edad"))
